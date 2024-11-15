ff7445e2c826a964acf0bd32dd07f2f400a072f7
kim-minsol <rlaalsthf02@naver.com>
2024-11-15 20:31:20 +0900

    [feat] #14 - config 모델 이름으로 자동 학습 진행

--- src/models/smp_models.py
+++ src/models/smp_models.py
@@ -18,16 +18,13 @@ class SMPSegmentationModel(nn.Module):
 
         architecture = self.cfg['MODEL'].get('ARCHITECTURE', 'Unet')
         
-        if architecture == 'Unet':
-            model = smp.Unet(**model_params)
-        elif architecture == 'DeepLabV3':
-            model = smp.DeepLabV3(**model_params)
-        elif architecture == 'DeepLabV3Plus':
-            model = smp.DeepLabV3Plus(**model_params)
-        elif architecture == 'FPN':
-            model = smp.FPN(**model_params)
-        else:
-            raise ValueError(f"Unsupported architecture: {architecture}")
+        # segmentation model pytorch
+        # deeplabv3, unet, unetplusplus, fpn, linknet, manet, pspnet, pan, upernet
+        try:
+            model_fn = getattr(smp, architecture)
+            model = model_fn(**model_params)
+        except AttributeError:
+            raise ValueError(f"Unsupported architecture: {architecture}. Please check if the model exists in segmentation_models_pytorch")
             
         return model
 
